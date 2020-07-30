b471f633eb28d51143dc0bd4fc4f0e0de175b6d1
den.is@rootmedia.ru <den.is@rootmedia.ru>
2020-07-30 09:48:41 +0300

    add menu

--- src/js/01_scripts/script.js
+++ src/js/01_scripts/script.js
@@ -1,26 +1,32 @@
 function canUseWebP() {
-    var elem = document.createElement("canvas");
-    if (!!(elem.getContext && elem.getContext("2d"))) {
-       return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
-    }
-    return false;
- }
+   var elem = document.createElement("canvas");
+   if (!!(elem.getContext && elem.getContext("2d"))) {
+      return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
+   }
+   return false;
+}
 
-$(document).ready(function(){
-    $(".intro__about-text, .intro__sentence-wrapper, .intro__hello-text").addClass("show");
-    
-    if(canUseWebP()) {
-        $("[data-img-name]").each(function(index,item) {
-           $(item).attr("data-href", "../img/" + $(item).attr("data-img-name") + ".webp")
-        });
-    }
-    else {
-        $("[data-img-name]").attr("data-href", "../min-img/" + $(this).attr("data-img-name") + "." + $(this).attr("data-name-ext"))
-    }
+$(document).ready(function () {
+   $(".intro__about-text, .intro__sentence-wrapper, .intro__hello-text").addClass("show");
 
-    $(".portfolio__btn-extend").on("click", function() {
-        $(this).attr("href",$(this).attr("data-href"));
+   if (canUseWebP()) {
+      $("[data-img-name]").each(function (index, item) {
+         $(item).attr("data-href", "../img/" + $(item).attr("data-img-name") + ".webp");
+      });
+   } else {
+      $("[data-img-name]").attr("data-href", "../min-img/" + $(this).attr("data-img-name") + "." + $(this).attr("data-name-ext"));
+   }
+
+   $(".portfolio__btn-extend").on("click", function () {
+      $(this).attr("href", $(this).attr("data-href"));
+   });
+
+   /* ------------- scroll ------------ */
+  
+    $("a[href^='#']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
     });
-
+   
 });
-
